e35ffa29ef06275d87817d52a1406b43173ff44d
panlf <liangfeng_pan@163.com>
2023-02-20 20:54:22 +0800

    补充Feign的内容

--- springboot-tutorial/springboot-feign/springboot-feign-client/src/main/java/com/plf/feign/client/config/FeignConfig.java
+++ springboot-tutorial/springboot-feign/springboot-feign-client/src/main/java/com/plf/feign/client/config/FeignConfig.java
@@ -1,6 +1,7 @@
 package com.plf.feign.client.config;
 
 import feign.Logger;
+import feign.Retryer;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
@@ -16,6 +17,12 @@ public class FeignConfig {
     @Bean
     Logger.Level feignLoggerLevel(){
         return Logger.Level.FULL;
+    }
+
+    // 重试机制，慎重，需要考虑接口幂等性
+    @Bean
+    Retryer feignRetryer() {
+        return new Retryer.Default();
     }
 
 }
--- springboot-tutorial/springboot-feign/springboot-feign-client/src/main/java/com/plf/feign/client/global/GlobalExceptionHandler.java
+++ springboot-tutorial/springboot-feign/springboot-feign-client/src/main/java/com/plf/feign/client/global/GlobalExceptionHandler.java
@@ -12,6 +12,16 @@ import java.net.SocketTimeoutException;
  */
 @ControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    @ResponseBody
+    public String ExceptionHandler(Exception e){
+        //feign.RetryableException 需要进一步判断
+        System.out.println(e.getClass().getName());
+        System.out.println(e.getMessage());
+        return "接口出错，请联系开发人员";
+    }
+
     /**
      * 这边应该是服务降级的代码。 简单的做法就是返回统一错误
      * @param e
@@ -19,7 +29,7 @@ public class GlobalExceptionHandler {
      */
     @ExceptionHandler(SocketTimeoutException.class)
     @ResponseBody
-    public String ExceptionHandler(Exception e){
+    public String SocketTimeoutExceptionHandler(Exception e){
         return "接口超时，请稍后再试";
     }
 }
