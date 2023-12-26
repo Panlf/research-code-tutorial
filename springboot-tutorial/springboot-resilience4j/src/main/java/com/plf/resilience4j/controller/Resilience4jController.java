fe74fe77e89af10eaffa1ca8dba51df42d022491
panlf <liangfeng_pan@163.com>
2023-12-26 23:09:34 +0800

    springboot整合Resilience4j（重试、断路、限流功能）

---
+++ springboot-tutorial/springboot-resilience4j/src/main/java/com/plf/resilience4j/controller/Resilience4jController.java
@@ -0,0 +1,55 @@
+package com.plf.resilience4j.controller;
+
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
+import io.github.resilience4j.retry.annotation.Retry;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Random;
+
+@RestController
+@Slf4j
+public class Resilience4jController {
+
+    @GetMapping("retry")
+    @Retry(name="retryApi",fallbackMethod="fallback")
+    public ResponseEntity<String> retryApi(){
+        log.info("request retryApi");
+        new RestTemplate().getForEntity("http://localhost:9090/getUser",String.class);
+        return new ResponseEntity<>("success",HttpStatus.OK);
+    }
+
+
+    @GetMapping("circuitBreaker")
+    @CircuitBreaker(name = "circuitBreakerApi",fallbackMethod = "fallback")
+    public ResponseEntity<String> circuitBreakerApi(){
+        log.info("request circuitBreakerApi");
+
+        int r = new Random().nextInt(100);
+
+        if(r >= 70){
+            throw new RuntimeException("circuitBreakerApi 访问出现异常");
+        }
+
+        return new ResponseEntity<>("success",HttpStatus.OK);
+    }
+
+    @GetMapping("ratelimit")
+    @RateLimiter(name="ratelimitApi",fallbackMethod = "fallback")
+    public ResponseEntity<String> ratelimitApi(){
+        log.info("request ratelimitApi");
+        return new ResponseEntity<>("success",HttpStatus.OK);
+    }
+
+
+    public ResponseEntity fallback(Throwable e){
+        log.error("fallback exception , {}",e.getMessage());
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+}
--- springboot-tutorial/springboot-unified-code/src/test/java/com/plf/boot/unified/common/ThreadTest.java
+++ springboot-tutorial/springboot-unified-code/src/test/java/com/plf/boot/unified/common/ThreadTest.java
@@ -4,9 +4,9 @@ import cn.hutool.http.HttpUtil;
 
 public class ThreadTest {
     public static void main(String[] args) {
-        for(int i=0;i<5;i++){
+        for(int i=0;i<3;i++){
             new Thread(()->{
-                String result = HttpUtil.get("http://localhost:9000/unified/get?id=1");
+                String result = HttpUtil.get("http://localhost:8080/ratelimit");
                 System.out.println(result);
             }).start();
         }
